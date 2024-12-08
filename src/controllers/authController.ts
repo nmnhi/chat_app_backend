532bddf0c0c3ae4dc8a40016d90b17fd1eb5297e
nmnhi <minhnhi08061105@gmail.com>
2024-12-08 20:51:50 +0700

    Implement auth controller

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -7,43 +7,50 @@ const SALT_ROUNDS = 10;
 
 const JWT_SECRET = process.env.JWT_SECRET || "worisecretkey";
 
-export const register = async (req: Request, res: Response) => {
-  // 1. get username, email, password
-  // 2. Insert those data into our db
-  // 3. return message, user
+export const register = async (req: Request, res: Response): Promise<any> => {
   const { username, email, password } = req.body;
+
   try {
-    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
-    const result = await pool.query(
-      "INSERT INTO users (username, email, password) VALUES($1, $2, $3) RETURNING *",
-      [username, email, hashedPassword]
-    );
-    const user = result.rows[0];
-    res.status(201).json({ message: "User registered successfully", user });
+    if (!username || !email || !password) {
+      return res
+        .status(400)
+        .json({ error: "username, email, or password is required" });
+    } else {
+      const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+      const result = await pool.query(
+        "INSERT INTO users (username, email, password) VALUES($1, $2, $3) RETURNING *",
+        [username, email, hashedPassword]
+      );
+      const user = result.rows[0];
+      res.status(201).json({ user });
+    }
   } catch (error) {
     res.status(500).json({ error: "Failed to register user" });
   }
 };
 
 export const login = async (req: Request, res: Response): Promise<any> => {
-  // 1. get email, password
-  // 2. verify email exist
-  // 3. compare password -> 'invalid credentials'
-  // 4. return token
   const { email, password } = req.body;
+
   try {
     const result = await pool.query("SELECT * FROM users WHERE email = $1", [
       email
     ]);
+
     const user = result.rows[0];
-    if (!user) return res.status(404).json({ error: "User not found" });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ error: "Invalid credentials" });
+    if (!isMatch) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
 
     const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: "10h" });
+    let finalResult = { ...user, token };
 
-    res.json({ message: "Logged in successfully", token });
+    res.status(200).json({ user: finalResult });
   } catch (error) {
     res.status(500).json({ error: "Failed to login" });
   }
